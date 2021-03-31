fb78333b4c70219c6aeef84bf94eeffae416b99f
lodenos <nikolaiilodenos@gmail.com>
2021-03-31 10:48:36 +0100

    save the draft

--- src/main.cr
+++ src/main.cr
@@ -1,1 +1,15 @@
-puts "wow"
+require "glfw"
+require "opengl"
+
+GLFW::Context.init
+
+window = GLFW::Window.new 1280, 720
+window.show
+
+
+
+while window.should_close == 0
+  LibGLFW.pollEvents
+end
+
+GLFW::Context.terminate
